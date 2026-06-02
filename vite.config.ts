f057416aa7e3bbe228e983f797fec9ef641b67c0
Polina Sloboda <31477024+slobodaPolina@users.noreply.github.com>
2026-06-02 15:36:55 +0300

    RD-X-check - add PWA (#6)
    Co-authored-by: Polina Sloboda <polina.sloboda@b2broker.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,40 @@
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/',
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        short_name: 'Daily App',
+        name: 'Daily App',
+        icons: [
+          {
+            src: './assets/logo512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+          {
+            src: './assets/logo192x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+      },
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
   css: {
     modules: {
       localsConvention: 'dashes',
